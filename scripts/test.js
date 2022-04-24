0f652fc8ac29681a75374642bbe1c7c683015df3
dancer1325 <alfredotic0809@gmail.com>
2022-04-24 23:57:29 +0200

    Add files to get the testing

---
+++ scripts/test.js
@@ -0,0 +1,55 @@
+// First dna generated is always 98500076866102862438511243873201350507055493981987690719374768781574322810666
+// Calculate dna's property
+
+// accessoriesType
+let remainder = 66 % 7;
+console.log("accessoriesType", remainder);
+
+// clotheColor
+remainder = 66 % 15;
+console.log("clotheColor", remainder);
+
+// clotheType
+remainder = 81 % 9;
+console.log("clotheType", remainder);
+
+// eyeType
+remainder = 22 % 12;
+console.log("eyeType", remainder);
+
+// eyebrowType
+remainder = 43 % 12;
+console.log("eyebrowType", remainder);
+
+// facialHairColor
+remainder = 57 % 8;
+console.log("facialHairColor", remainder);
+
+// facialHairType
+remainder = 81 % 6;
+console.log("facialHairType", remainder);
+
+// hairColor
+remainder = 87 % 10;
+console.log("hairColor", remainder);
+
+// hatColor
+remainder = 76 % 15;
+console.log("hatColor", remainder);
+
+// graphicType
+remainder = 74 % 11;
+console.log("graphicType", remainder);
+
+// mouthType
+remainder = 93 % 12;
+console.log("mouthType", remainder);
+
+// skinColor
+remainder = 71 % 7;
+console.log("skinColor", remainder);
+
+// topType
+remainder = 90 % 35;
+console.log("topType", remainder);
+
